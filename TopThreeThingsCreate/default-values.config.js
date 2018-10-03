f91e2cf18d2667a6db34efe009824b81d9373c8a
bogdan_scher <bsc@piogroup.com>
2018-10-03 17:34:09 +0300

    default values

---
+++ TopThreeThingsCreate/default-values.config.js
@@ -0,0 +1,10 @@
+import { valuesNames } from '../forms.config';
+
+export const defaultFormValues = {
+  [valuesNames.NAME1]: 'To be completed',
+  [valuesNames.NAME2]: 'To be completed',
+  [valuesNames.NAME3]: 'To be completed',
+  [valuesNames.DESCRIPTION1]: 'To be completed',
+  [valuesNames.DESCRIPTION2]: 'To be completed',
+  [valuesNames.DESCRIPTION3]: 'To be completed',
+};
